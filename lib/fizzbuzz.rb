678ea5677732db70e0e6c423eb9cc7c1c930f3b3
John Ashton <John_ashton@hotmail.com>
2016-12-15 18:20:12 +0000

    Additional changes to create functioning IRB fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(number)
-  if number % 3 == 0 ; "Fizz"
-  elsif number % 5 == 0 ; "Buzz"
+  if number % 3 == 0 && number % 5 !=0 ; "Fizz"
+  elsif number % 5 == 0 && number % 3 !=0 ; "Buzz"
   elsif number % 3 && number % 5 != 0  ; "#{number}"
   else number % 3 == 0 && number % 5 == 0 ; "Fizzbuzz"
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizz" when divisible 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+  it 'returns "Buzz" when divisible by 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+  it 'returns "Fizzbuzz" when divisible by both 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'Fizzbuzz'
+  end
+  it 'returns the number when divisible by neither 3 or 5' do
+    expect(fizzbuzz(4)).to eq '4'
   end
 end
