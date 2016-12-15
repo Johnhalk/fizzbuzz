0d794c3c362c3bb983673c0c3b96c617f32716be
John Ashton <John_ashton@hotmail.com>
2016-12-15 18:05:27 +0000

    Upgraded for Fizz, buzz and fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,7 @@
 def fizzbuzz(number)
-  'fizz'
-
+  if number % 3 == 0 ; "Fizz"
+  elsif number % 5 == 0 ; "Buzz"
+  elsif number % 3 && number % 5 != 0  ; "#{number}"
+  else number % 3 == 0 && number % 5 == 0 ; "Fizzbuzz"
+end
 end
