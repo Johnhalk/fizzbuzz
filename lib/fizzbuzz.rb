6e167f465e29d5507cc8567ef1fb8dbd37114630
John Ashton <John_ashton@hotmail.com>
2016-12-15 18:22:00 +0000

    Fizzbuzz functionality

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 !=0 ; "Fizz"
   elsif number % 5 == 0 && number % 3 !=0 ; "Buzz"
-  elsif number % 3 && number % 5 != 0  ; "#{number}"
+  elsif number % 3 !=0 && number % 5 != 0  ; "#{number}"
   else number % 3 == 0 && number % 5 == 0 ; "Fizzbuzz"
 end
 end
